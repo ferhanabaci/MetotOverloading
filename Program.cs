671a82e1e843e7e642f371b255d1eccf3b0a5496
Ferhan Abaci <ferhanabacii@gmail.com>
2022-02-27 00:45:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace metot_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // out parametreler 
+            string sayi = "999";
+            int outSayi;
+            bool sonuc =int.TryParse(sayi, out  outSayi);
+            if(sonuc){
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+
+            }else{
+                Console.WriteLine("Başarısız");
+
+            }
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplamSonucu );
+            Console.WriteLine(toplamSonucu);
+
+            //Metot overloading -Metot aşırı yükleme
+            //biz herhanhangi fonk imzasını degiştirerek birden fazla sekilde o fonk kullanabiliriz
+
+            int ifade=999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Ferhan","Abaci");
+            // buna nasıl karar veriyor metot imazsına göre
+            //metotAdı +parametre sayisi+ parametre tip 
+    }
+    class Metotlar{
+        public void Topla(int a,int b,out int toplam){
+            toplam=a+b;
+        }
+        public void EkranaYazdir(int veri){
+            Console.WriteLine(veri);
+        }
+         public void EkranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+         public void EkranaYazdir(string veri1,string veri2){
+            Console.WriteLine(veri1+veri2);
+        }
+    }
+}
+}
+
